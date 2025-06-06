624f5c6d9f1c1f7680f9b14fc6e7a8c51a4e0207
mrlongruoi <mrlong01012020@gmail.com>
2025-06-06 23:36:19 +0700

    01: setup env-auth-modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
+import {
+  ClerkProvider,
+} from '@clerk/nextjs'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ModalProvider } from "@/providers/modal-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +27,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ModalProvider/>
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
